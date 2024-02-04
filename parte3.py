852af715b832cd81460b6f2423c21931ff226e53
Emma <emma.gatto.04@gmail.com>
2024-02-04 15:36:00 +0100

    parte 3

--- parte3.py
+++ parte3.py
@@ -2,6 +2,25 @@
 #print(list[0:1])
 
 
+
 file = open('shampoo_sales.csv', 'r')
-print(file.read()[0:3])
+file_content= file.read()
+if len(file_content) > 50:
+    print(file_content[0:50] + '...')
+else:
+    print(file_content)
 file.close()
+
+file = open('shampoo_sales.csv', 'r')
+print(file.readline())
+print(file.readline())
+file.close()
+
+file = open('shampoo_sales.csv', 'r')
+for line in file:
+    print(line)
+file.close()
+
+my_file = open('saluti.text', 'w')
+my_file.write('hello world')
+my_file.close()
